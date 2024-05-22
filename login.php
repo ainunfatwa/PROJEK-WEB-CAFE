fedd08ce84316243b87f8533731d1bb92eed13b9
ainunfatwa <153829520+ainunfatwa@users.noreply.github.com>
2024-05-22 22:26:00 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,13 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Sistem</title>
-</head>
-<body>
-    <h2>Login Sistem</h2>
-    <form action="cek_login.php" method="Post">
-        Username : <input type="text" name="username"><p>
-        Password : <input type="submit" value="Log In">
-    </form>
-</body>
-</html>
+<?php
+    // koneksi ke database
+     $username = "root";
+     $password = "";
+     $dbname = "project_web2";
+     $conn = new mysqli($servername, $username, $password, $dbname);
+         if ($conn->connect_error) {
+         die("Connection failed: " . $conn->connect_error);
+         }
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+    // mengecek apakah email dan password cocok
+     $result = $conn->query("SELECT * FROM mahasiswa WHERE email='$email' AND 
+            password='$password'");
+     if ($result->num_rows == 0) {
+    // mengecek apakah email terdaftar
+     $check_email = $conn->query("SELECT * FROM mahasiswa WHERE email='$email'");
+     if ($check_email->num_rows == 0) {
+     echo "Password tidak cocok";
+    } else {
+     echo "Akun tidak terdaftar";
+    }
+    } else {
+    // memulai sesi
+     session_start();
+     while ($row = $result->fetch_assoc()) {
+     // mengatur variabel sesi
+     $_SESSION['username'] = $row['username'];
+     $_SESSION['email'] = $row['email'];
+     $_SESSION['password'] = $row['password'];
+     }
+     header('Location: beranda.php');
+     }
+?>
+
