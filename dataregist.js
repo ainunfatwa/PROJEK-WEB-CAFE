f57576810fa5577a7197a7a073dc1383293905f6
ainunfatwa <153829520+ainunfatwa@users.noreply.github.com>
2024-06-06 22:37:47 +0800

    Add files via upload

---
+++ dataregist.js
@@ -0,0 +1,28 @@
+const daftar_regist = [
+    {
+        id: 1,
+        nama: "SITTI NURHALWA RUSDI",
+        tannggal_reservasi: "2024-04-2",
+        waktu: "08.00",
+        jumlah_orang: "9",
+        no_telp: "089788002341",
+    },
+    {
+        id: 2,
+        nama: "Muhammad Alfin Khalilullah",
+        tannggal_reservasi: "2024-05-19",
+        waktu: "17.00",
+        jumlah_orang: "5",
+        no_telp: "081221445678",
+
+    },
+    {
+        id: 3,
+        nama: "MUHAMMAD IHKSAN",
+        tannggal_reservasi: "2024-02-20",
+        waktu: "16.00",
+        jumlah_orang: "6",
+        no_telp: "087766521432",
+    },
+    
+]
